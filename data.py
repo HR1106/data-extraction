1160769606981fd2debde99d78b2039eb4815f1d
HR1106 <54427897+HR1106@users.noreply.github.com>
2020-07-01 22:06:21 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,26 +1,24 @@
 import io
 from PIL import Image
+import fitz
 import pytesseract
-from pdf2image import convert_from_path
-pdf="hr1.pdf"
-pages = convert_from_path(pdf)
-image_counter = 1 
-for page in pages: 
-  
-    filename = "page_"+str(image_counter)+".jpg"
-      
-    page.save(filename, 'JPEG') 
-    
-    image_counter = image_counter + 1
-filelimit = image_counter-1
+file=fitz.open("39_Previous_Papers_Indian_History_CSAT_Paper_I_Civil_Services_Exam.pdf")
+page=len(file)
+for i in range(2,page):
+  for image in file.getPageImageList(i):
+    j=0
+    customxref=image[0]
+    pic=fitz.Pixmap(file,customxref)
+    finalpic=fitz.Pixmap(fitz.csRGB,pic)
+    finalpic.writePNG(str(i)+".png")
+    pic=None
+    finalpic=None
 outfile = "out_text.txt"
 f = open(outfile, "a")
-for i in range(1, filelimit + 1):  
-    filename = "page_"+str(i)+".jpg"
+for i in range(2,page):  
+    filename = str(i)+".png"
            
-    text = str(((pytesseract.image_to_string(Image.open(filename))))) 
-  
-    text = text.replace('-\n', '')     
+    text = str(((pytesseract.image_to_string(Image.open(filename)))))  
   
     f.write(text)
 f.close()
