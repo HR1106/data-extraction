cffab29c3b221e1937589508640552287c894ea1
HR1106 <54427897+HR1106@users.noreply.github.com>
2020-07-01 11:23:22 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,9 @@
+import os
+import pdfplumber
+
+os.system(f'ocrmypdf --skip-text 39_Previous_Papers_Indian_History_CSAT_Paper_I_Civil_Services_Exam.pdf output.pdf')
+
+with pdfplumber.open('output.pdf') as pdf:
+  page=pdf.pages[3]
+  text=page.extract_text(x_tolerance=2)
+  print(text)
