604020b2966abf4e9640d27521237a734029325b
HR1106 <54427897+HR1106@users.noreply.github.com>
2020-07-02 14:53:21 +0530

    Create filter.py

---
+++ filter.py
@@ -0,0 +1,11 @@
+import re
+import fileinput
+for line in fileinput.input("output.txt", inplace=True):
+    print(re.sub(r'Page\s\d+', "", line))
+for line in fileinput.input("output.txt", inplace=True):
+    print(re.sub(r'\d{2,}[.:]', "", line))
+for line in fileinput.input("output.txt", inplace=True):
+    print(re.sub(r'\w+\sServic.+\w+\s\d+', "", line))
+for line in fileinput.FileInput("output.txt",inplace=1):
+    if line.rstrip():
+        print(line)
