50253a82a52e789d7f08e7fb5f930c1c728e5eb0
HR1106 <54427897+HR1106@users.noreply.github.com>
2020-07-02 15:24:08 +0530

    Update filter.py

--- filter.py
+++ filter.py
@@ -5,7 +5,7 @@ for line in fileinput.input("output.txt", inplace=True):
 for line in fileinput.input("output.txt", inplace=True):
     print(re.sub(r'\d{2,}[.:]', "", line))
 for line in fileinput.input("output.txt", inplace=True):
-    print(re.sub(r'\w+\sServic.+\w+\s\d+', "", line))
+    print(re.sub(r'\w+\sServic.+\w+.\d+', "", line))
 for line in fileinput.FileInput("output.txt",inplace=1):
     if line.rstrip():
         print(line)
